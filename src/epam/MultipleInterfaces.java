0dd3866bc25b0c638b58fea0e553dd1ec7ccf3ac
Pavan753 <83013713+Pavan753@users.noreply.github.com>
2024-07-15 10:35:51 +0530

    Add files via upload

---
+++ src/epam/MultipleInterfaces.java
@@ -0,0 +1,36 @@
+package epam;
+interface Shape{
+	double getarea();
+}
+interface Color{
+	String getcolor();
+}
+class Rectangle implements Shape,Color{
+	double width;
+	double length;
+	String color;
+	double area;
+	public Rectangle(double width,double length, String color) { //constructor.
+		this.width = width;
+		this.length = length;
+		this.color = color;	
+	}
+    public double getarea() {
+    	area = width * length;
+    	return area;
+    }
+    public String getcolor() {
+    	return color;
+    }
+}
+public class MultipleInterfaces {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Rectangle rectangle = new Rectangle(2,4,"Black");
+		double area = rectangle.getarea();
+		System.out.println(area);
+		String color = rectangle.getcolor();
+		System.out.println(color);	
+	}
+}
